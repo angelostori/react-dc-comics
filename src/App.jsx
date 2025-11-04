035f77a00e0583864cdb817a466b1809aa0e9ff0
angelostori <angelostori13@gmail.com>
2025-11-05 00:10:42 +0100

    componentize the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,13 @@
 import Header from './assets/components/header'
 import Main from './assets/components/Main'
-import digitalComicsImg from './assets/img/buy-comics-digital-comics.png'
-import merchandiseImg from './assets/img/buy-comics-merchandise.png'
-import shopLocatorImg from './assets/img/buy-comics-shop-locator.png'
-import subscriptionsImg from './assets/img/buy-comics-subscriptions.png'
-import dcPowerVisaImg from './assets/img/buy-dc-power-visa.svg'
-
-import footerLogoImg from './assets/img/dc-logo-bg.png'
-
-import facebookLogo from './assets/img/footer-facebook.png'
-import twitterLogo from './assets/img/footer-twitter.png'
-import youtubeLogo from './assets/img/footer-youtube.png'
-import pinterestLogo from './assets/img/footer-pinterest.png'
-import periscopeLogo from './assets/img/footer-periscope.png'
+import Footer from './assets/components/Footer'
 function App() {
 
   return (
     <>
       <Header />
       <Main />
-      <footer>
-        <section class='footer-top' >
-          <div class="container">
-            <ul class="nav justify-content-around align-items-center">
-              <li class="nav-item">
-                <img src={digitalComicsImg} alt="" />
-                <a class="nav-link" href="#">DIGITAL COMICS</a>
-              </li>
-              <li class="nav-item">
-                <img src={merchandiseImg} alt="" />
-                <a class="nav-link" href="#">DC MERCHANDISE</a>
-              </li>
-              <li class="nav-item">
-                <img src={shopLocatorImg} alt="" />
-                <a class="nav-link" href="#">COMIC SHOP LOCATOR</a>
-              </li>
-              <li class="nav-item">
-                <img src={subscriptionsImg} alt="" />
-                <a class="nav-link" href="#">SUBSCRIPTION</a>
-              </li>
-              <li class="nav-item">
-                <img src={dcPowerVisaImg} alt="" />
-                <a class="nav-link" href="#">DC POWER VISA</a>
-              </li>
-            </ul>
-          </div>
-
-        </section>
-
-        <section className="footer-mid">
-          <div className="container d-flex justify-content-between">
-            <ul class="nav">
-              <li class="nav-item">
-                <label><strong>DC COMICS</strong></label>
-                <a class="nav-link" href="#">Characters</a>
-                <a class="nav-link" href="#">Comics</a>
-                <a class="nav-link" href="#">Movies</a>
-                <a class="nav-link" href="#">TV</a>
-                <a class="nav-link" href="#">Games</a>
-                <a class="nav-link" href="#">Videos</a>
-                <a class="nav-link" href="#">News</a>
-                <label><strong>SHOP</strong></label>
-                <a class="nav-link" href="#">Shop DC</a>
-                <a class="nav-link" href="#">Shop DC Collectibles</a>
-              </li>
-
-              <li class="nav-item">
-                <label><strong>DC</strong></label>
-                <a class="nav-link" href="#">Terms Of Use</a>
-                <a class="nav-link" href="#">Privacy policy(New)</a>
-                <a class="nav-link" href="#">Ad Choices</a>
-                <a class="nav-link" href="#">Advertising</a>
-                <a class="nav-link" href="#">Jobs</a>
-                <a class="nav-link" href="#">Subscribtions</a>
-                <a class="nav-link" href="#">Talent Workshops</a>
-                <a class="nav-link" href="#">CPSC Certificates</a>
-                <a class="nav-link" href="#">Ratings</a>
-                <a class="nav-link" href="#">Shop Help</a>
-                <a class="nav-link" href="#">Contact Us</a>
-              </li>
-
-              <li class="nav-item">
-                <label><strong>SITES</strong></label>
-                <a className="nav-link" href="#">DC</a>
-                <a className="nav-link" href="#">MAD Magazine</a>
-                <a className="nav-link" href="#">DC Kids</a>
-                <a className="nav-link" href="#">DC Universe</a>
-                <a className="nav-link" href="#">DC Power Visa</a>
-              </li>
-            </ul>
-
-            <img src={footerLogoImg} alt="" />
-          </div>
-        </section>
-
-        <section className='footer-bottom'>
-          <div className="container">
-            <nav class="nav justify-content-between">
-              <button type="button">SIGN UP NOW!</button>
-              <div>
-                <label><strong>FOLLOW US</strong></label>
-                <a class="nav-link" href="#">
-                  <img src={facebookLogo} alt="" />
-                </a>
-                <a class="nav-link" href="#">
-                  <img src={twitterLogo} alt="" />
-                </a>
-                <a class="nav-link" href="#">
-                  <img src={youtubeLogo} alt="" />
-                </a>
-                <a class="nav-link" href="#">
-                  <img src={pinterestLogo} alt="" />
-                </a>
-                <a class="nav-link" href="#">
-                  <img src={periscopeLogo} alt="" />
-                </a>
-              </div>
-
-            </nav>
-          </div>
-        </section>
-
-      </footer>
+      <Footer />
     </>
   )
 }
---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,122 @@
+import digitalComicsImg from '../img/buy-comics-digital-comics.png'
+import merchandiseImg from '../img/buy-comics-merchandise.png'
+import shopLocatorImg from '../img/buy-comics-shop-locator.png'
+import subscriptionsImg from '../img/buy-comics-subscriptions.png'
+import dcPowerVisaImg from '../img/buy-dc-power-visa.svg'
+
+import footerLogoImg from '../img/dc-logo-bg.png'
+
+import facebookLogo from '../img/footer-facebook.png'
+import twitterLogo from '../img/footer-twitter.png'
+import youtubeLogo from '../img/footer-youtube.png'
+import pinterestLogo from '../img/footer-pinterest.png'
+import periscopeLogo from '../img/footer-periscope.png'
+
+export default function Footer() {
+
+    return (
+        <footer>
+            <section className='footer-top' >
+                <div className="container">
+                    <ul className="nav justify-content-around align-items-center">
+                        <li className="nav-item">
+                            <img src={digitalComicsImg} alt="" />
+                            <a className="nav-link" href="#">DIGITAL COMICS</a>
+                        </li>
+                        <li className="nav-item">
+                            <img src={merchandiseImg} alt="" />
+                            <a className="nav-link" href="#">DC MERCHANDISE</a>
+                        </li>
+                        <li className="nav-item">
+                            <img src={shopLocatorImg} alt="" />
+                            <a className="nav-link" href="#">COMIC SHOP LOCATOR</a>
+                        </li>
+                        <li className="nav-item">
+                            <img src={subscriptionsImg} alt="" />
+                            <a className="nav-link" href="#">SUBSCRIPTION</a>
+                        </li>
+                        <li className="nav-item">
+                            <img src={dcPowerVisaImg} alt="" />
+                            <a className="nav-link" href="#">DC POWER VISA</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </section>
+
+            <section className="footer-mid">
+                <div className="container d-flex justify-content-between">
+                    <ul className="nav">
+                        <li className="nav-item">
+                            <label><strong>DC COMICS</strong></label>
+                            <a className="nav-link" href="#">Characters</a>
+                            <a className="nav-link" href="#">Comics</a>
+                            <a className="nav-link" href="#">Movies</a>
+                            <a className="nav-link" href="#">TV</a>
+                            <a className="nav-link" href="#">Games</a>
+                            <a className="nav-link" href="#">Videos</a>
+                            <a className="nav-link" href="#">News</a>
+                            <label><strong>SHOP</strong></label>
+                            <a className="nav-link" href="#">Shop DC</a>
+                            <a className="nav-link" href="#">Shop DC Collectibles</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <label><strong>DC</strong></label>
+                            <a className="nav-link" href="#">Terms Of Use</a>
+                            <a className="nav-link" href="#">Privacy policy(New)</a>
+                            <a className="nav-link" href="#">Ad Choices</a>
+                            <a className="nav-link" href="#">Advertising</a>
+                            <a className="nav-link" href="#">Jobs</a>
+                            <a className="nav-link" href="#">Subscribtions</a>
+                            <a className="nav-link" href="#">Talent Workshops</a>
+                            <a className="nav-link" href="#">CPSC Certificates</a>
+                            <a className="nav-link" href="#">Ratings</a>
+                            <a className="nav-link" href="#">Shop Help</a>
+                            <a className="nav-link" href="#">Contact Us</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <label><strong>SITES</strong></label>
+                            <a className="nav-link" href="#">DC</a>
+                            <a className="nav-link" href="#">MAD Magazine</a>
+                            <a className="nav-link" href="#">DC Kids</a>
+                            <a className="nav-link" href="#">DC Universe</a>
+                            <a className="nav-link" href="#">DC Power Visa</a>
+                        </li>
+                    </ul>
+
+                    <img src={footerLogoImg} alt="" />
+                </div>
+            </section>
+
+            <section className='footer-bottom'>
+                <div className="container">
+                    <nav className="nav justify-content-between">
+                        <button type="button">SIGN UP NOW!</button>
+                        <div>
+                            <label><strong>FOLLOW US</strong></label>
+                            <a className="nav-link" href="#">
+                                <img src={facebookLogo} alt="" />
+                            </a>
+                            <a className="nav-link" href="#">
+                                <img src={twitterLogo} alt="" />
+                            </a>
+                            <a className="nav-link" href="#">
+                                <img src={youtubeLogo} alt="" />
+                            </a>
+                            <a className="nav-link" href="#">
+                                <img src={pinterestLogo} alt="" />
+                            </a>
+                            <a className="nav-link" href="#">
+                                <img src={periscopeLogo} alt="" />
+                            </a>
+                        </div>
+
+                    </nav>
+                </div>
+            </section>
+
+        </footer>
+    )
+}
