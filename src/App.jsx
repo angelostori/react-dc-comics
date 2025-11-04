9bde6736d46e25a7710037721870ee6ed8cd592b
angelostori <angelostori13@gmail.com>
2025-11-04 16:34:38 +0100

    define the main macroareas in app.jsx, clear index.css and install bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import dcLogo from './assets/img/dc-logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <img src={dcLogo} alt="" />
+      <header>
+        <img className='logo' width={150} src={dcLogo} alt="" />
+      </header>
+      <main></main>
+      <footer></footer>
     </>
   )
 }
