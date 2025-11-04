75dc171e0d95ab44633abd0e3e5306c5119be259
angelostori <angelostori13@gmail.com>
2025-11-04 22:13:21 +0100

    make the footer mid

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import shopLocatorImg from './assets/img/buy-comics-shop-locator.png'
 import subscriptionsImg from './assets/img/buy-comics-subscriptions.png'
 import dcPowerVisaImg from './assets/img/buy-dc-power-visa.svg'
 
+import footerLogoImg from './assets/img/dc-logo-bg.png'
 function App() {
 
   return (
@@ -41,7 +42,55 @@ function App() {
 
         </section>
 
+        <section className="footer-mid">
+          <div className="container d-flex justify-content-between">
+            <ul class="nav">
+              <li class="nav-item">
+                <label><strong>DC COMICS</strong></label>
+                <a class="nav-link" href="#">Characters</a>
+                <a class="nav-link" href="#">Comics</a>
+                <a class="nav-link" href="#">Movies</a>
+                <a class="nav-link" href="#">TV</a>
+                <a class="nav-link" href="#">Games</a>
+                <a class="nav-link" href="#">Videos</a>
+                <a class="nav-link" href="#">News</a>
+                <label><strong>SHOP</strong></label>
+                <a class="nav-link" href="#">Shop DC</a>
+                <a class="nav-link" href="#">Shop DC Collectibles</a>
+              </li>
 
+              <li class="nav-item">
+                <label><strong>DC</strong></label>
+                <a class="nav-link" href="#">Terms Of Use</a>
+                <a class="nav-link" href="#">Privacy policy(New)</a>
+                <a class="nav-link" href="#">Ad Choices</a>
+                <a class="nav-link" href="#">Advertising</a>
+                <a class="nav-link" href="#">Jobs</a>
+                <a class="nav-link" href="#">Subscribtions</a>
+                <a class="nav-link" href="#">Talent Workshops</a>
+                <a class="nav-link" href="#">CPSC Certificates</a>
+                <a class="nav-link" href="#">Ratings</a>
+                <a class="nav-link" href="#">Shop Help</a>
+                <a class="nav-link" href="#">Contact Us</a>
+              </li>
+
+              <li class="nav-item">
+                <label><strong>SITES</strong></label>
+                <a className="nav-link" href="#">DC</a>
+                <a className="nav-link" href="#">MAD Magazine</a>
+                <a className="nav-link" href="#">DC Kids</a>
+                <a className="nav-link" href="#">DC Universe</a>
+                <a className="nav-link" href="#">DC Power Visa</a>
+              </li>
+            </ul>
+
+            <img src={footerLogoImg} alt="" />
+          </div>
+        </section>
+
+        <section className=''>
+
+        </section>
       </footer>
     </>
   )
