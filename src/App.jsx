5bed534e3a511ce423103d1ec4b2f6819cbe4d48
angelostori <angelostori13@gmail.com>
2025-11-04 18:32:58 +0100

    define the header's style and import the google font

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,41 @@ function App() {
   return (
     <>
       <header>
-
-        <nav class="nav justify-content-around">
-          <img className='logo' width={60} src={dcLogo} alt="" />
-
-          <a class="nav-link" href="#">CHARACTERS</a>
-          <a class="nav-link" href="#">COMICS</a>
-          <a class="nav-link" href="#">MOVIES</a>
-          <a class="nav-link" href="#">TV</a>
-          <a class="nav-link" href="#">GAMES</a>
-          <a class="nav-link" href="#">COLLECTIBLES</a>
-          <a class="nav-link" href="#">VIDEOS</a>
-          <a class="nav-link" href="#">FANS</a>
-          <a class="nav-link" href="#">NEWS</a>
-          <a class="nav-link" href="#">SHOP</a>
+        <nav class="d-flex justify-content-between align-items-center">
+          <img class="logo" width="60" src={dcLogo} alt="DC Logo" />
+          <div class="d-flex gap-3">
+            <div>
+              <a class="nav-link" href="#">CHARACTERS</a>
+            </div>
+            <div>
+              <a class="nav-link active" href="#">COMICS</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">MOVIES</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">TV</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">GAMES</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">COLLECTIBLES</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">VIDEOS</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">FANS</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">NEWS</a>
+            </div>
+            <div>
+              <a class="nav-link" href="#">SHOP</a>
+            </div>
+          </div>
         </nav>
-
       </header>
       <main></main>
       <footer></footer>
