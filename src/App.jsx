3e95c7bc6d5be17b925830b90c1644fa62bcd726
angelostori <angelostori13@gmail.com>
2025-11-04 17:13:22 +0100

    insert in page a primitive nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,22 @@ function App() {
   return (
     <>
       <header>
-        <img className='logo' width={150} src={dcLogo} alt="" />
+
+        <nav class="nav justify-content-around">
+          <img className='logo' width={60} src={dcLogo} alt="" />
+
+          <a class="nav-link" href="#">CHARACTERS</a>
+          <a class="nav-link" href="#">COMICS</a>
+          <a class="nav-link" href="#">MOVIES</a>
+          <a class="nav-link" href="#">TV</a>
+          <a class="nav-link" href="#">GAMES</a>
+          <a class="nav-link" href="#">COLLECTIBLES</a>
+          <a class="nav-link" href="#">VIDEOS</a>
+          <a class="nav-link" href="#">FANS</a>
+          <a class="nav-link" href="#">NEWS</a>
+          <a class="nav-link" href="#">SHOP</a>
+        </nav>
+
       </header>
       <main></main>
       <footer></footer>
