c73faec0db66ce11a2343fe5df2b57a95023202d
angelostori <angelostori13@gmail.com>
2025-11-05 13:05:50 +0100

    make some style to the cards

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,57 +1,6 @@
 import { comics } from "../../../public/comics.js";
 
 export default function Main() {
-    const menu = [
-
-        {
-
-            id: 1,
-
-            link: '#',
-
-            text: 'Home',
-
-            is_active: false
-
-        },
-
-        {
-
-            id: 2,
-
-            link: '#',
-
-            text: 'Prodotti',
-
-            is_active: true
-
-        },
-
-        {
-
-            id: 3,
-
-            link: '#',
-
-            text: 'Chi Siamo',
-
-            is_active: false
-
-        },
-
-        {
-
-            id: 4,
-
-            link: '#',
-
-            text: 'Contatti',
-
-            is_active: false
-
-        },
-
-    ]
 
     return (
         <main>
@@ -74,14 +23,6 @@ export default function Main() {
                     </div>
                 </div>
             </section>
-
-            <nav className="nav justify-content-center">
-                {
-                    menu.map(item => (
-                        <a key={item.id} className={`nav-link ${item.is_active ? 'current' : ''}`} href={item.link}>{item.text}</a>
-                    ))
-                }
-            </nav>
         </main>
     )
 }
