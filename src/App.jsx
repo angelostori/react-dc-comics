61de3b3e42834d9d2b19701cf7322e51a921ba0d
angelostori <angelostori13@gmail.com>
2025-11-06 09:28:56 +0100

    change the icon in top

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './assets/components/header'
+import Header from './assets/components/Header'
 import Main from './assets/components/Main'
 import Navbar from './assets/components/Navbar'
 import Footer from './assets/components/Footer'
